c575d4b7ba8598e9b481b3c40cd9ed8a982739b3
jacqueline-homan <jacquelinehoman7@gmail.com>
2015-05-31 19:25:21 -0400

    first commit

---
+++ Program.fs
@@ -0,0 +1,26 @@
+﻿//For all natural numbers n below 1000,
+//list all the multiples of 3 and 5.
+
+// Types we want to define first
+// i.e. does it take an argument?
+
+
+
+let filter(n:seq<int>):seq<int> =
+    seq { for i in n do 
+            if i % 3 = 0 || i % 5 = 0 then
+                yield i}
+
+let sum(n:seq<int>):int =
+    Seq.sum(n)
+
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (filter [1..9])
+    printfn "Sum of the sequence: %d" (filter [1..9] |> sum)
+    printfn "%A" (filter [1..999]) 
+    printfn "Sum of the sequence: %d" (filter [1..999] |> sum)
+    0 // return an integer exit code
+
